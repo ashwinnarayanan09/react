b2bca68b8c6e0a087a1485a0fd8806c4d81a9325
ashwin <ashwinnarayanan09@gmail.com>
2019-10-18 14:32:39 +0200

    redux 1

--- react-redux-tutorial/src/js/actions/index.js
+++ react-redux-tutorial/src/js/actions/index.js
@@ -1,5 +1,6 @@
-import { ADD_ARTICLE } from "../constants/action-types";
+
+//Action Creator
 
 export function addArticle(payload) {
-  return { type: ADD_ARTICLE, payload };
-}
+  return { type: "ADD_ARTICLE", payload }
+};
